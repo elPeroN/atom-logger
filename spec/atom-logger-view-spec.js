56f8c029cccf8cef020346f2d5f96de9a8135fb2
elpero <peropix@hotmail.it>
2020-02-12 12:10:27 +0100

    Plugin di base

---
+++ spec/atom-logger-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomLoggerView from '../lib/atom-logger-view';
+
+describe('AtomLoggerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
